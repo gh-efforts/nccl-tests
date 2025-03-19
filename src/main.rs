7a24243e978eaa229ef2b7facad55d87df8e4c59
xutianyi <xutianyi1999@live.com>
2025-03-19 18:09:45 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use candle_core::{CudaDevice, Device, Tensor};
 use candle_core::backend::BackendDevice;
 
-// same node, GPU0 -> mem -> GPU1
+// same node, GPU0 -> CPU mem -> GPU1
 fn t1() -> Result<()>{
     let core_0 = CudaDevice::new(0)?;
     let core_1 = CudaDevice::new(1)?;
@@ -11,10 +11,13 @@ fn t1() -> Result<()>{
     let core_0 = Device::Cuda(core_0);
     let core_1 = Device::Cuda(core_1);
 
+    let t1x8 = Tensor::arange(0f32, 64f32, &core_0)?;
+    t1x8.device().synchronize()?;
+
     let t = Instant::now();
-    let t1x8 = Tensor::arange(0f32, 8f32, &core_0)?;
     let t1x8 = t1x8.to_device(&core_1)?;
     t1x8.device().synchronize()?;
+
     println!("use {:?}", t.elapsed());
     Ok(())
 }
