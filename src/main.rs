3130d4c70aa0286977eb95a6b32b284b35573bb2
xutianyi <xutianyi1999@live.com>
2025-03-21 14:06:03 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,61 @@
 use std::time::Instant;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use candle_core::{CudaDevice, Device, Tensor};
 use candle_core::backend::BackendDevice;
 
-// same node, GPU0 -> CPU mem -> GPU1
-fn t1() -> Result<()>{
+// same node, same numa, GPU0 -> CPU mem -> GPU1
+fn t1(n: f32) -> Result<()>{
     let core_0 = CudaDevice::new(0)?;
     let core_1 = CudaDevice::new(1)?;
 
     let core_0 = Device::Cuda(core_0);
     let core_1 = Device::Cuda(core_1);
 
-    let t1x8 = Tensor::arange(0f32, 64f32, &core_0)?;
+    let x = Tensor::arange(0f32, n, &core_0)?;
+    x.device().synchronize()?;
 
     let t = Instant::now();
-    let t1x8 = t1x8.to_device(&core_1)?;
-    t1x8.device().synchronize()?;
+    let x= x.to_device(&core_1)?;
+    x.device().synchronize()?;
 
-    println!("use {:?}", t.elapsed());
+    println!("same node, GPU0 -> CPU mem -> GPU1, use {:?}", t.elapsed());
+    Ok(())
+}
+
+// same node, cross numa, GPU0 -> CPU mem -> GPU1
+fn t2(n: f32) -> Result<()>{
+    let core_0 = CudaDevice::new(0)?;
+    let core_1 = CudaDevice::new(7)?;
+
+    let core_0 = Device::Cuda(core_0);
+    let core_1 = Device::Cuda(core_1);
+
+    let x = Tensor::arange(0f32, n, &core_0)?;
+    x.device().synchronize()?;
+
+    let t = Instant::now();
+    let x= x.to_device(&core_1)?;
+    x.device().synchronize()?;
+
+    println!("same node, GPU0 -> CPU mem -> GPU7, use {:?}", t.elapsed());
+    Ok(())
+}
+
+fn t3(n: f32) -> Result<()> {
+    let core_0 = CudaDevice::new(0)?;
+    let core_1 = CudaDevice::new(1)?;
+
+    let core_0 = core_0.cuda_device();
+    let core_1 = core_1.cuda_device();
+
+    let devices = vec![core_0, core_1];
+    let comms = cudarc::nccl::Comm::from_devices(devices).map_err(|e| anyhow!("{:?}", e))?;
+
+    let s = comms[0].world_size();
+    println!("{}", s);
     Ok(())
 }
 
 fn main() {
-    t1().unwrap();
+    t3(10f32).unwrap();
 }
