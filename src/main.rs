51c2d9aac518fcfe250678c5f1c5a48b133f80a5
xutianyi <xutianyi1999@live.com>
2025-03-24 14:25:29 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
-use std::time::Instant;
 use anyhow::{anyhow, Result};
-use candle_core::{CudaDevice, Device, Tensor};
 use candle_core::backend::BackendDevice;
+use candle_core::{
+    CpuStorage, CudaDevice, CudaStorage, DType, Device, InplaceOp1, Layout, Storage, Tensor,
+};
+use cudarc::nccl::{Comm, Id};
+use std::time::Instant;
+use candle_core::cuda::CudaStorageSlice;
+
+struct TensorCopy {
+    comm: Comm,
+    from: i32,
+}
+
+impl InplaceOp1 for TensorCopy {
+    fn name(&self) -> &'static str {
+        "tensor-copy"
+    }
+
+    fn cpu_fwd(&self, storage: &mut CpuStorage, layout: &Layout) -> candle_core::Result<()> {
+        todo!()
+    }
+
+    fn cuda_fwd(&self, storage: &mut CudaStorage, _layout: &Layout) -> candle_core::Result<()> {
+        let slice = match &mut storage.slice {
+            CudaStorageSlice::F32(v) => v,
+            _ => unreachable!()
+        };
+
+        self.comm
+            .recv(slice, self.from)
+            .unwrap();
+        Ok(())
+    }
+}
 
 // same node, same numa, GPU0 -> CPU mem -> GPU1
-fn t1(n: f32) -> Result<()>{
+fn t1(n: f32) -> Result<()> {
     let core_0 = CudaDevice::new(0)?;
     let core_1 = CudaDevice::new(1)?;
 
@@ -15,7 +46,7 @@ fn t1(n: f32) -> Result<()>{
     x.device().synchronize()?;
 
     let t = Instant::now();
-    let x= x.to_device(&core_1)?;
+    let x = x.to_device(&core_1)?;
     x.device().synchronize()?;
 
     println!("same node, GPU0 -> CPU mem -> GPU1, use {:?}", t.elapsed());
@@ -23,7 +54,7 @@ fn t1(n: f32) -> Result<()>{
 }
 
 // same node, cross numa, GPU0 -> CPU mem -> GPU1
-fn t2(n: f32) -> Result<()>{
+fn t2(n: f32) -> Result<()> {
     let core_0 = CudaDevice::new(0)?;
     let core_1 = CudaDevice::new(7)?;
 
@@ -34,7 +65,7 @@ fn t2(n: f32) -> Result<()>{
     x.device().synchronize()?;
 
     let t = Instant::now();
-    let x= x.to_device(&core_1)?;
+    let x = x.to_device(&core_1)?;
     x.device().synchronize()?;
 
     println!("same node, GPU0 -> CPU mem -> GPU7, use {:?}", t.elapsed());
@@ -42,17 +73,47 @@ fn t2(n: f32) -> Result<()>{
 }
 
 fn t3(n: f32) -> Result<()> {
+    let id = Id::new().unwrap();
+
     let core_0 = CudaDevice::new(0)?;
     let core_1 = CudaDevice::new(1)?;
 
-    let core_0 = core_0.cuda_device();
-    let core_1 = core_1.cuda_device();
+    let core_0_raw = core_0.cuda_device();
+    let core_1_raw = core_1.cuda_device();
+    let comm = cudarc::nccl::Comm::from_rank(core_0_raw, 0, 2, id).unwrap();
 
-    let devices = vec![core_0, core_1];
-    let comms = cudarc::nccl::Comm::from_devices(devices).map_err(|e| anyhow!("{:?}", e))?;
+    let core_0 = Device::Cuda(core_0);
 
-    let s = comms[0].world_size();
-    println!("{}", s);
+    let x = Tensor::arange(0f32, n, &core_0)?;
+    let x_count = x.elem_count();
+
+    let h = std::thread::spawn({
+        move || {
+            let comm = cudarc::nccl::Comm::from_rank(core_1_raw, 1, 2, id).unwrap();
+
+            let core_1 = Device::Cuda(core_1);
+            let mut op = TensorCopy { comm, from: 0 };
+            let t = Tensor::zeros((x_count, 1), DType::F32, &core_1)?;
+            t.inplace_op1(&mut op)?;
+            t.device().synchronize()?;
+
+            println!("{}", t);
+            Result::<_, anyhow::Error>::Ok(())
+        }
+    });
+    let (data, layout) = x.storage_and_layout();
+
+    let s = match &(*data) {
+        Storage::Cuda(s) => s,
+        _ => unreachable!(),
+    };
+
+    let data = s.as_cuda_slice::<f32>()?;
+    comm
+        .send(data, 1)
+        .map_err(|e| anyhow!("{:?}", e))?;
+
+    h.join().unwrap().unwrap();
     Ok(())
 }
 
