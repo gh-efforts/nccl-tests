2780be93dee32c7ae2e0c8bd1619fe59cb53528d
xutianyi <xutianyi1999@live.com>
2025-03-25 09:49:07 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env::args;
 use anyhow::{anyhow, ensure, Result};
 use candle_core::backend::BackendDevice;
 use candle_core::cuda::CudaStorageSlice;
@@ -104,7 +105,7 @@ fn t2<S: Into<Shape> + Copy + Send + 'static>(shape: S, core0: usize, core1: usi
         }
     });
 
-    let core_0 = CudaDevice::new(0)?;
+    let core_0 = CudaDevice::new(core0)?;
     let core_0_raw = core_0.cuda_device();
     let core_0 = Device::Cuda(core_0);
     let comm = Comm::from_rank(core_0_raw, 0, 2, id).unwrap();
@@ -141,20 +142,141 @@ fn t2<S: Into<Shape> + Copy + Send + 'static>(shape: S, core0: usize, core1: usi
     Ok(())
 }
 
+fn t3<S: Into<Shape> + Copy + Send + 'static>(shape: S, core0: usize, id_idx: u8) -> Result<()> {
+    let mut id_arr = [0i8; 128];
+
+    let id = std::fs::read(format!("id{}.dat", id_idx))?;
+    for (i, &x) in id.iter().enumerate() {
+        id_arr[i] = x as i8;
+    }
+
+    let id = Id::uninit(id_arr);
+
+    let core_0 = CudaDevice::new(core0)?;
+    let core_0_raw = core_0.cuda_device();
+    let core_0 = Device::Cuda(core_0);
+    let comm = Comm::from_rank(core_0_raw, 0, 2, id).unwrap();
+
+    let mut op = TensorCopy { comm: &comm, from: 1 };
+
+    let x = Tensor::rand::<_, f32>(0f32, 100f32, shape.clone(), &core_0)?;
+    x.device().synchronize()?;
+
+    let recv_t = Tensor::zeros(shape, DType::F32, &core_0)?;
+    recv_t.device().synchronize()?;
+
+    let t = Instant::now();
+    let (data, _layout) = x.storage_and_layout();
+    let s = match &(*data) {
+        Storage::Cuda(s) => s,
+        _ => unreachable!(),
+    };
+    let data = s.as_cuda_slice::<f32>()?;
+    comm
+        .send(data, 1)
+        .map_err(|e| anyhow!("{:?}", e))?;
+    recv_t.inplace_op1(&mut op)?;
+    recv_t.device().synchronize()?;
+    let elapsed = t.elapsed();
+
+    let a = Tensor::full(1f32, shape, &core_0)?;
+    let x = x.add(&a)?;
+
+    ensure!(recv_t.to_string() == x.to_string());
+    println!("use nccl, shape {:?}, dtype {}, node0 -> node1, use {:?}", x.shape(), "f32", elapsed);
+    Ok(())
+}
+
+fn t3_daemon<S: Into<Shape> + Copy + Send + 'static>(shape: S, core1: usize, id_idx: u8) -> Result<()> {
+    let mut id_arr = [0i8; 128];
+
+    let id = std::fs::read(format!("id{}.dat", id_idx))?;
+    for (i, &x) in id.iter().enumerate() {
+        id_arr[i] = x as i8;
+    }
+
+    let id = Id::uninit(id_arr);
+
+    let core_1 = CudaDevice::new(core1)?;
+    let core_1_raw = core_1.cuda_device();
+    let comm = Comm::from_rank(core_1_raw, 1, 2, id).map_err(|_| anyhow!("nccl error"))?;
+    let core_1 = Device::Cuda(core_1);
+
+    let a = Tensor::full(1f32, shape, &core_1)?;
+    a.device().synchronize()?;
+
+    let mut op = TensorCopy { comm: &comm, from: 0 };
+    let t = Tensor::zeros(shape, DType::F32, &core_1)?;
+    t.device().synchronize()?;
+
+    println!("serve idx {} started", id_idx);
+
+    loop {
+        t.inplace_op1(&mut op)?;
+        let out = t.add(&a)?;
+        out.device().synchronize()?;
+        let (data, _) = out.storage_and_layout();
+        let s = match &(*data) {
+            Storage::Cuda(s) => s,
+            _ => unreachable!(),
+        };
+        let data = s.as_cuda_slice::<f32>()?;
+        comm.send(data, 0)
+            .map_err(|e| anyhow!("{:?}", e))?;
+    }
+}
+
+fn create_nccl_id(idx: u8) {
+    let id = Id::new().unwrap();
+    let id = id.internal();
+    let id_bytes = id.iter().map(|&v| v as u8).collect::<Vec<_>>();
+    std::fs::write(format!("id{}.dat", idx), id_bytes).unwrap();
+}
+
 fn main() {
-    t1((2, 4), 0, 1).unwrap();
-    t1((2048, 4096), 0, 1).unwrap();
-    t1((2048 * 8, 4096 * 8), 0, 1).unwrap();
+    let mut args = args();
+    args.next();
 
-    t1((2, 4), 0, 7).unwrap();
-    t1((2048, 4096), 0, 7).unwrap();
-    t1((2048 * 8, 4096 * 8), 0, 7).unwrap();
+    match args.next().as_deref() {
+        None => {
+            t1((2, 4), 0, 1).unwrap();
+            t1((2048, 4096), 0, 1).unwrap();
+            t1((2048 * 8, 4096 * 8), 0, 1).unwrap();
 
-    t2((2, 4), 0, 1).unwrap();
-    t2((2048, 4096), 0, 1).unwrap();
-    t2((2048 * 8, 4096 * 8), 0, 1).unwrap();
+            t1((2, 4), 0, 7).unwrap();
+            t1((2048, 4096), 0, 7).unwrap();
+            t1((2048 * 8, 4096 * 8), 0, 7).unwrap();
 
-    t2((2, 4), 0, 7).unwrap();
-    t2((2048, 4096), 0, 7).unwrap();
-    t2((2048 * 8, 4096 * 8), 0, 7).unwrap();
+            t2((2, 4), 0, 1).unwrap();
+            t2((2048, 4096), 0, 1).unwrap();
+            t2((2048 * 8, 4096 * 8), 0, 1).unwrap();
+
+            t2((2, 4), 0, 7).unwrap();
+            t2((2048, 4096), 0, 7).unwrap();
+            t2((2048 * 8, 4096 * 8), 0, 7).unwrap();
+
+            t3((2, 4), 0, 0).unwrap();
+            t3((2048, 4096), 0, 1).unwrap();
+            t3((2048 * 8, 4096 * 8), 0, 2).unwrap();
+        }
+        Some("genid") => {
+            create_nccl_id(0);
+            create_nccl_id(1);
+            create_nccl_id(2);
+        }
+        Some("daemon") => {
+            std::thread::scope(|s| {
+                s.spawn(|| {
+                    t3_daemon((2, 4), 0, 0).unwrap();
+                });
+                s.spawn(|| {
+                    t3_daemon((2048, 4096), 0, 1).unwrap();
+                });
+                s.spawn(|| {
+                    t3_daemon((2048 * 8, 4096 * 8), 0, 2).unwrap();
+                });
+            })
+        }
+        _ => panic!("unknown command"),
+    }
 }
